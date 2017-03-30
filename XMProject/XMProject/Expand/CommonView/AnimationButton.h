3d9dfe29bf51453024b619d9db4eb393b68c4465
wukexiu <290348331@qq.com>
2017-03-30 16:51:49 +0800

    no message

---
+++ XMProject/XMProject/Expand/CommonView/AnimationButton.h
@@ -0,0 +1,19 @@
+//
+//  AnimationButton.h
+//  XMProject
+//
+//  Created by wukexiu on 17/3/30.
+//  Copyright © 2017年 wukexiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AnimationButton : UIButton
+
+-(void)startAnimation;
+
+-(void)stopAnimation;
+
+-(void)resumeAnimation;
+
+@end
